d0db4619217c392c03a5a2e230a9389854e803b8
Aziz Namazov <anamazov@amazon.com>
2020-08-11 21:49:59 +1000

    Add cars

---
+++ lesson5_1/src/carsclasses/Driveable.java
@@ -0,0 +1,5 @@
+package carsclasses;
+
+public interface Driveable {
+    public void drive();
+}
---
+++ lesson5_1/src/carsclasses/Main.java
@@ -0,0 +1,33 @@
+package carsclasses;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Car mazdaCx5 = new CombustionCar("Mazda", 120, 2200, CarType.SUV);
+        Car bmwm3 = new CombustionCar("BMW M3", 220, 1800, CarType.COUPE);
+        Car tesla = new ElectricCar("Tesla", 250, 2000, CarType.SEDAN);
+
+        testTheCar(mazdaCx5);
+        testTheCar(bmwm3);
+        testTheCar(tesla);
+    }
+
+    public static void testTheCar(Car car){
+        System.out.println("--------------");
+        describeCar(car);
+        driveCar(car);
+        System.out.println("--------------");
+
+    }
+
+    public static void describeCar(Car car){
+        System.out.println("Name: " + car.getName() + "\n" +
+                "Type: " + car.getCarType() + "\n" +
+                "HP: " + car.getHp() + "\n" +
+                "Weight: " + car.getWeight());
+    }
+
+    public static void driveCar(Driveable driveable){
+        driveable.drive();
+    }
+}
